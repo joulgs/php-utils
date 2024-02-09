eba0589e2f1a503b69250eb33cda43773864d4e7
Jonathas Soares <joul.gs@gmail.com>
2024-02-09 15:01:37 -0300

    remove NIG Angolano

--- src/FiscalDocuments.php
+++ src/FiscalDocuments.php
@@ -7,8 +7,6 @@ class FiscalDocuments
     public $doc_number;
     public $type;
     public const DOC_TYPE_NIF_POR = 'NIF';
-    // removido pos não tem uma forma sempre correta de ser validado: https://github.com/assoft-portugal/SAF-T-AO/discussions/132
-    // public const DOC_TYPE_NIF_ANG = 'NIF_ANG'; 
     public const DOC_TYPE_CPF = 'CPF';
     public const DOC_TYPE_CNPJ = 'CNPJ';
 
@@ -22,8 +20,6 @@ class FiscalDocuments
         switch (strlen($this->doc_number)) {
             case 9:
                 return self::DOC_TYPE_NIF_POR;
-                // case 10:
-                //     return self::DOC_TYPE_NIF_ANG;
             case 11:
                 return self::DOC_TYPE_CPF;
             case 14:
@@ -42,8 +38,6 @@ class FiscalDocuments
         switch ($type) {
             case self::DOC_TYPE_NIF_POR:
                 return $this->validateNIF();
-                // case self::DOC_TYPE_NIF_ANG:
-                //     return $this->validateNIFAng();
             case self::DOC_TYPE_CPF:
                 return $this->validateCPF();
             case self::DOC_TYPE_CNPJ:
@@ -54,30 +48,6 @@ class FiscalDocuments
     public function isInvalid()
     {
         return !$this->isValid();
-    }
-
-    private function validateNIFAng()
-    {
-        $nif = $this->doc_number;
-
-        $nif_split = str_split($nif);
-
-        $nif_primeiros_digito = array(1, 2, 3, 5, 6, 7, 8, 9);
-
-        if (is_numeric($nif) && strlen($nif) == 10 && in_array($nif_split[0], $nif_primeiros_digito)) {
-            $check_digit = 0;
-            for ($i = 0; $i < 9; $i++) {
-                $check_digit += $nif_split[$i] * (11 - $i - 1);
-            }
-
-            $check_digit = 12 - ($check_digit % 12);
-            $check_digit = $check_digit >= 11 ? 0 : $check_digit;
-
-            if ($check_digit == $nif_split[9])
-                return true;
-        }
-
-        return false;
     }
 
     private function validateNIF()
